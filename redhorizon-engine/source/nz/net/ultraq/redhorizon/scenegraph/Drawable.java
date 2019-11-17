a043bffdefef7d1c4bd04f829c473a885f04f4b4
Emanuel Rabina <emanuelrabina@gmail.com>
2019-11-17 19:01:32 +1300

    Shhhh - not needed for now

--- redhorizon-engine/source/nz/net/ultraq/redhorizon/scenegraph/Drawable.java
+++ redhorizon-engine/source/nz/net/ultraq/redhorizon/scenegraph/Drawable.java
@@ -16,7 +16,7 @@
 
 package nz.net.ultraq.redhorizon.scenegraph;
 
-import redhorizon.engine.graphics.GraphicsObject;
+//import redhorizon.engine.graphics.GraphicsObject;
 
 /**
  * Interface to define methods for objects that can be drawn onto the
@@ -25,23 +25,23 @@ import redhorizon.engine.graphics.GraphicsObject;
  * 
  * @author Emanuel Rabina
  */
-public interface Drawable extends GraphicsObject {
-
-	/**
-	 * Requests that the object be drawn from the next rendering pass onwards.
-	 */
-	public void draw();
-
-	/**
-	 * Requests that the object stop being drawn from the next rendering pass
-	 * onwards.
-	 */
-	public void erase();
-
-	/**
-	 * Queries whether this item is being drawn.
-	 * 
-	 * @return <tt>true</tt> if it is to be drawn, <tt>false</tt> otherwise.
-	 */
-	public boolean isDrawing();
-}
+//public interface Drawable extends GraphicsObject {
+//
+//	/**
+//	 * Requests that the object be drawn from the next rendering pass onwards.
+//	 */
+//	public void draw();
+//
+//	/**
+//	 * Requests that the object stop being drawn from the next rendering pass
+//	 * onwards.
+//	 */
+//	public void erase();
+//
+//	/**
+//	 * Queries whether this item is being drawn.
+//	 * 
+//	 * @return <tt>true</tt> if it is to be drawn, <tt>false</tt> otherwise.
+//	 */
+//	public boolean isDrawing();
+//}
