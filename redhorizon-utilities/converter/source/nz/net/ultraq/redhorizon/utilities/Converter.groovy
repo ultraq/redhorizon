7ae6c7c97e11b5076ab5b2bfbcdcbc6616fc5d5a
Emanuel Rabina <emanuelrabina@gmail.com>
2020-01-27 22:13:04 +1300

    Make the converter a single application with subcommands

---
+++ redhorizon-utilities/converter/source/nz/net/ultraq/redhorizon/utilities/Converter.groovy
@@ -0,0 +1,74 @@
+/* 
+ * Copyright 2020, Emanuel Rabina (http://www.ultraq.net.nz/)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package nz.net.ultraq.redhorizon.utilities
+
+import nz.net.ultraq.redhorizon.utilities.converter.Pcx2CpsConverter
+
+import picocli.CommandLine
+import picocli.CommandLine.Command
+import picocli.CommandLine.Model.CommandSpec
+import picocli.CommandLine.ParameterException
+import picocli.CommandLine.Spec
+
+import java.util.concurrent.Callable
+
+/**
+ * CLI tool for converting from one file format to another.
+ * 
+ * @author Emanuel Rabina
+ */
+@Command(
+	name = 'convert',
+	header = [
+		'',
+		'Red Horizon Utilities - File Converter',
+		'======================================',
+		''
+	],
+	mixinStandardHelpOptions = true,
+	subcommands = [
+	  Pcx2CpsConverter
+	],
+	synopsisSubcommandLabel = 'COMMAND',
+	version = '${sys:redhorizon.version}'
+)
+class Converter implements Callable<Integer> {
+
+	@Spec
+	CommandSpec commandSpec
+
+	/**
+	 * If invoked, then a subcommand hasn't been specified and so an error is
+	 * thrown.
+	 * 
+	 * @return
+	 */
+	@Override
+	Integer call() {
+
+		throw new ParameterException(commandSpec.commandLine(), 'Conversion subcommand required')
+	}
+
+	/**
+	 * Bootstrap the application using Picocli.
+	 * 
+	 * @param args
+	 */
+	static void main(String[] args) {
+		System.exit(new CommandLine(new Converter()).execute(args))
+	}
+}
--- redhorizon-utilities/converter/source/nz/net/ultraq/redhorizon/utilities/converter/Pcx2CpsConverter.groovy
+++ redhorizon-utilities/converter/source/nz/net/ultraq/redhorizon/utilities/converter/Pcx2CpsConverter.groovy
@@ -21,36 +21,23 @@ import nz.net.ultraq.redhorizon.filetypes.pcx.PcxFile
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import picocli.CommandLine
 import picocli.CommandLine.Command
-import picocli.CommandLine.Model.CommandSpec
 import picocli.CommandLine.Parameters
-import picocli.CommandLine.Spec
 
 import java.util.concurrent.Callable
 
 /**
- * CLI tool for converting PCX files to C&C CPS files.
+ * Converter subcommand for converting PCX files to C&C CPS files.
  * 
  * @author Emanuel Rabina
  */
 @Command(
-	name = 'mix',
-	header = [
-		'',
-		'Red Horizon Utilities - PCX file to CPS file converter',
-		'======================================================',
-		''
-	],
-	mixinStandardHelpOptions = true,
-	version = '${sys:redhorizon.version}'
+	name = 'pcx2cps',
+	description = 'Convert a PCX file to a Command & Conquer CPS file'
 )
 class Pcx2CpsConverter implements Callable<Integer> {
 
 	private static final Logger logger = LoggerFactory.getLogger(Pcx2CpsConverter)
-
-	@Spec
-	CommandSpec commandSpec
 
 	@Parameters(index = '0', arity = '1', description = 'A 320x200 PCX image.  If creating a paletted CPS, then the PCX file must have an internal palette.')
 	File sourceFile
@@ -65,8 +52,6 @@ class Pcx2CpsConverter implements Callable<Integer> {
 	 */
 	@Override
 	Integer call() {
-
-		logger.info('Red Horizon Utilities - PCX -> CPS file converter {}', commandSpec.version()[0] ?: '(development)')
 
 		logger.info('Loading {}...', sourceFile)
 		if (sourceFile.exists()) {
@@ -87,14 +72,5 @@ class Pcx2CpsConverter implements Callable<Integer> {
 			return 1
 		}
 		return 0
-	}
-
-	/**
-	 * Bootstrap the application using Picocli.
-	 * 
-	 * @param args
-	 */
-	static void main(String[] args) {
-		System.exit(new CommandLine(new Pcx2CpsConverter()).execute(args))
 	}
 }
