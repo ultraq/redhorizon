ed4afa19288e81368dfb2d6778dbb0f07e3bc57a
Emanuel Rabina <emanuelrabina@gmail.com>
2021-09-04 19:52:11 +1200

    sineComp can be calculated statically

--- redhorizon-engine/source/nz/net/ultraq/redhorizon/engine/graphics/opengl/Scanlines.frag.glsl
+++ redhorizon-engine/source/nz/net/ultraq/redhorizon/engine/graphics/opengl/Scanlines.frag.glsl
@@ -3,6 +3,7 @@
 #define scanlineBaseBrightness float(1.0)
 #define scanlineHorizontalModulation float(0.0)
 #define scanlineVerticalModulation float(0.15)
+#define sineComp vec2(scanlineHorizontalModulation, scanlineVerticalModulation)
 
 in vec4 v_vertexColour;
 in vec2 v_textureUVs;
@@ -23,7 +24,6 @@ uniform sampler2D textures[MAX_TEXTURE_UNITS];
 void main() {
 
 	fragmentColour = texture(textures[int(v_textureUnit)], v_textureUVs);
-	vec2 sineComp = vec2(scanlineHorizontalModulation, scanlineVerticalModulation);
 	fragmentColour *= vec4(scanlineBaseBrightness + dot(sineComp * sin(v_textureUVs * v_omega - 1), vec2(1.0, 1.0)));
 	fragmentColour *= v_vertexColour;
 }
