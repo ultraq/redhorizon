027e8358df8bef85e757403600f19da454d4c5a0
Emanuel Rabina <emanuelrabina@gmail.com>
2025-09-06 12:01:40 +1200

    Move Ogg decoder to be part of the audio module

---
+++ redhorizon-audio/source/nz/net/ultraq/redhorizon/audio/OggAudioDecoder.groovy
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2025, Emanuel Rabina (http://www.ultraq.net.nz/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package nz.net.ultraq.redhorizon.audio
+
+import java.nio.ByteBuffer
+import javax.sound.sampled.AudioFormat.Encoding
+import javax.sound.sampled.AudioSystem
+
+/**
+ * A sound decoder for OGG files.  This implementation is a wrapper around the
+ * OGG decoding provided by the <a href="https://github.com/hendriks73/ffsampledsp">ffsamplessp</a>
+ * project, which in turn provides the decoder for use with the
+ * {@code javax.sound.samples} package.
+ *
+ * @author Emanuel Rabina
+ */
+class OggAudioDecoder implements AudioDecoder {
+
+	final String[] supportedFileExtensions = ['ogg']
+
+	@Override
+	DecodeSummary decode(InputStream inputStream) {
+
+		def (bits, channels, frequency, numSamples) = AudioSystem.getAudioInputStream(inputStream).withCloseable { oggStream ->
+			return AudioSystem.getAudioInputStream(Encoding.PCM_SIGNED, oggStream).withCloseable { pcmStream ->
+				var format = pcmStream.format
+				var bits = format.sampleSizeInBits
+				var channels = format.channels
+				var frequency = (int)format.sampleRate
+				var frameLength = pcmStream.frameLength
+				var numFrames = 0
+
+				// No frame length, read everything in one go
+				if (frameLength == AudioSystem.NOT_SPECIFIED) {
+					trigger(new SampleDecodedEvent(bits, channels, frequency, ByteBuffer.wrapNative(pcmStream.readAllBytes())))
+					numFrames = 1
+				}
+				// Decode frame-by-frame
+				else {
+					while (numFrames < frameLength && !Thread.interrupted()) {
+						var frameBytes = new byte[format.frameSize]
+						pcmStream.read(frameBytes)
+						trigger(new SampleDecodedEvent(bits, channels, frequency, ByteBuffer.wrapNative(frameBytes)))
+						numFrames++
+						Thread.yield()
+					}
+				}
+
+				return new Tuple4<Integer, Integer, Integer, Integer>(bits, channels, frequency, numFrames)
+			}
+		}
+
+		return new DecodeSummary(bits, channels, frequency, numSamples,
+			"Ogg file, ${frequency}hz ${bits}-bit ${channels == 2 ? 'Stereo' : 'Mono'}")
+	}
+}
