8f12475a818a03f590afaf94d76908f785ba0536
Emanuel Rabina <emanuelrabina@gmail.com>
2026-06-25 21:54:00 +1200

    Try update gamepad mappings

--- redhorizon-graphics/source/nz/net/ultraq/redhorizon/graphics/input/GamepadStateProcessor.groovy
+++ redhorizon-graphics/source/nz/net/ultraq/redhorizon/graphics/input/GamepadStateProcessor.groovy
@@ -14,8 +14,9 @@
  * limitations under the License.
  */
 
-package nz.net.ultraq.redhorizon.graphics.opengl
+package nz.net.ultraq.redhorizon.graphics.input
 
+import nz.net.ultraq.redhorizon.graphics.opengl.OpenGLWindow
 import nz.net.ultraq.redhorizon.input.GamepadAxisEvent
 import nz.net.ultraq.redhorizon.input.GamepadButtonEvent
 
@@ -24,7 +25,6 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import static org.lwjgl.glfw.GLFW.*
 
-import groovy.transform.TupleConstructor
 import java.nio.ByteBuffer
 import java.nio.FloatBuffer
 import java.util.concurrent.ConcurrentHashMap
@@ -43,7 +43,6 @@ import java.util.concurrent.ConcurrentHashMap
  *
  * @author Emanuel Rabina
  */
-@TupleConstructor(defaults = false)
 class GamepadStateProcessor {
 
 	private static final Logger logger = LoggerFactory.getLogger(GamepadStateProcessor)
@@ -52,6 +51,29 @@ class GamepadStateProcessor {
 	@Lazy
 	private GLFWGamepadState gamepadState = { GLFWGamepadState.create() }()
 	private final Map<Integer, Boolean> buttonStates = new ConcurrentHashMap<>()
+
+	/**
+	 * Constructor, sets up gamepad processing.
+	 */
+	GamepadStateProcessor(OpenGLWindow window) {
+
+		this.window = window
+
+		// Update the controller DB - the one in GLFW hasn't been updated since 2021 😮
+		// https://github.com/glfw/glfw/pull/2745
+//		stackPush().withCloseable { stack ->
+//		var gameControllerDb = getResourceAsText('nz/net/ultraq/redhorizon/graphics/input/gamecontrollerdb.txt')
+//		var gameControllerDb = '030000005e040000050b000003090000,Xbox Elite Controller Series 2,a:b0,b:b1,back:b31,dpdown:h0.4,dpleft:h0.8,dpright:h0.2,dpup:h0.1,guide:b53,leftshoulder:b6,leftstick:b13,lefttrigger:a6,leftx:a0,lefty:a1,rightshoulder:b7,rightstick:b14,righttrigger:a5,rightx:a2,righty:a3,start:b11,x:b3,y:b4,platform:Mac OS X'
+//		var buffer = stackASCII(gameControllerDb)
+//			var buffer = stack.malloc(gameControllerDb.length() + Long.BYTES)
+//				.put(gameControllerDb.getBytes())
+//				.putLong(NULL)
+//				.flip()
+//		glfwUpdateGamepadMappings(buffer)
+//		}
+
+		logger.debug('Gamepad name: {} / {}', glfwGetJoystickName(GLFW_JOYSTICK_1), glfwGetGamepadName(GLFW_JOYSTICK_1))
+	}
 
 	/**
 	 * Check for any changes to the joystick/gamepad state and emit events for
@@ -106,40 +128,40 @@ class GamepadStateProcessor {
 		// Testing has shown that not all of the buttons in GLFW match exactly to
 		// the buttons on a gamepad on macOS, so this remaps the GLFW names to the
 		// values experienced so that we can continue to use GLFW constants.
-		if (System.isMacOs()) {
-			processButton(buttons, GLFW_GAMEPAD_BUTTON_A, 0)
-			processButton(buttons, GLFW_GAMEPAD_BUTTON_B, 1)
-			processButton(buttons, GLFW_GAMEPAD_BUTTON_X, 3)
-			processButton(buttons, GLFW_GAMEPAD_BUTTON_Y, 4)
-			processButton(buttons, GLFW_GAMEPAD_BUTTON_LEFT_BUMPER, 6)
-			processButton(buttons, GLFW_GAMEPAD_BUTTON_RIGHT_BUMPER, 7)
-			processButton(buttons, GLFW_GAMEPAD_BUTTON_BACK, 10)
-			processButton(buttons, GLFW_GAMEPAD_BUTTON_START, 11)
-			processButton(buttons, GLFW_GAMEPAD_BUTTON_GUIDE, 12)
-			processButton(buttons, GLFW_GAMEPAD_BUTTON_LEFT_THUMB, 13)
-			processButton(buttons, GLFW_GAMEPAD_BUTTON_RIGHT_THUMB, 14)
-			processButton(buttons, GLFW_GAMEPAD_BUTTON_DPAD_UP, 23)
-			processButton(buttons, GLFW_GAMEPAD_BUTTON_DPAD_RIGHT, 24)
-			processButton(buttons, GLFW_GAMEPAD_BUTTON_DPAD_DOWN, 25)
-			processButton(buttons, GLFW_GAMEPAD_BUTTON_DPAD_LEFT, 26)
-		}
-		else {
-			processButton(buttons, GLFW_GAMEPAD_BUTTON_A, GLFW_GAMEPAD_BUTTON_A)
-			processButton(buttons, GLFW_GAMEPAD_BUTTON_B, GLFW_GAMEPAD_BUTTON_B)
-			processButton(buttons, GLFW_GAMEPAD_BUTTON_X, GLFW_GAMEPAD_BUTTON_X)
-			processButton(buttons, GLFW_GAMEPAD_BUTTON_Y, GLFW_GAMEPAD_BUTTON_Y)
-			processButton(buttons, GLFW_GAMEPAD_BUTTON_LEFT_BUMPER, GLFW_GAMEPAD_BUTTON_LEFT_BUMPER)
-			processButton(buttons, GLFW_GAMEPAD_BUTTON_RIGHT_BUMPER, GLFW_GAMEPAD_BUTTON_RIGHT_BUMPER)
-			processButton(buttons, GLFW_GAMEPAD_BUTTON_BACK, GLFW_GAMEPAD_BUTTON_BACK)
-			processButton(buttons, GLFW_GAMEPAD_BUTTON_START, GLFW_GAMEPAD_BUTTON_START)
-			processButton(buttons, GLFW_GAMEPAD_BUTTON_GUIDE, GLFW_GAMEPAD_BUTTON_GUIDE)
-			processButton(buttons, GLFW_GAMEPAD_BUTTON_LEFT_THUMB, GLFW_GAMEPAD_BUTTON_LEFT_THUMB)
-			processButton(buttons, GLFW_GAMEPAD_BUTTON_RIGHT_THUMB, GLFW_GAMEPAD_BUTTON_RIGHT_THUMB)
-			processButton(buttons, GLFW_GAMEPAD_BUTTON_DPAD_UP, GLFW_GAMEPAD_BUTTON_DPAD_UP)
-			processButton(buttons, GLFW_GAMEPAD_BUTTON_DPAD_RIGHT, GLFW_GAMEPAD_BUTTON_DPAD_RIGHT)
-			processButton(buttons, GLFW_GAMEPAD_BUTTON_DPAD_DOWN, GLFW_GAMEPAD_BUTTON_DPAD_DOWN)
-			processButton(buttons, GLFW_GAMEPAD_BUTTON_DPAD_LEFT, GLFW_GAMEPAD_BUTTON_DPAD_LEFT)
-		}
+//		if (System.isMacOs()) {
+//			processButton(buttons, GLFW_GAMEPAD_BUTTON_A, 0)
+//			processButton(buttons, GLFW_GAMEPAD_BUTTON_B, 1)
+//			processButton(buttons, GLFW_GAMEPAD_BUTTON_X, 3)
+//			processButton(buttons, GLFW_GAMEPAD_BUTTON_Y, 4)
+//			processButton(buttons, GLFW_GAMEPAD_BUTTON_LEFT_BUMPER, 6)
+//			processButton(buttons, GLFW_GAMEPAD_BUTTON_RIGHT_BUMPER, 7)
+//			processButton(buttons, GLFW_GAMEPAD_BUTTON_BACK, 10)
+//			processButton(buttons, GLFW_GAMEPAD_BUTTON_START, 11)
+//			processButton(buttons, GLFW_GAMEPAD_BUTTON_GUIDE, 12)
+//			processButton(buttons, GLFW_GAMEPAD_BUTTON_LEFT_THUMB, 13)
+//			processButton(buttons, GLFW_GAMEPAD_BUTTON_RIGHT_THUMB, 14)
+//			processButton(buttons, GLFW_GAMEPAD_BUTTON_DPAD_UP, 23)
+//			processButton(buttons, GLFW_GAMEPAD_BUTTON_DPAD_RIGHT, 24)
+//			processButton(buttons, GLFW_GAMEPAD_BUTTON_DPAD_DOWN, 25)
+//			processButton(buttons, GLFW_GAMEPAD_BUTTON_DPAD_LEFT, 26)
+//		}
+//		else {
+		processButton(buttons, GLFW_GAMEPAD_BUTTON_A, GLFW_GAMEPAD_BUTTON_A)
+		processButton(buttons, GLFW_GAMEPAD_BUTTON_B, GLFW_GAMEPAD_BUTTON_B)
+		processButton(buttons, GLFW_GAMEPAD_BUTTON_X, GLFW_GAMEPAD_BUTTON_X)
+		processButton(buttons, GLFW_GAMEPAD_BUTTON_Y, GLFW_GAMEPAD_BUTTON_Y)
+		processButton(buttons, GLFW_GAMEPAD_BUTTON_LEFT_BUMPER, GLFW_GAMEPAD_BUTTON_LEFT_BUMPER)
+		processButton(buttons, GLFW_GAMEPAD_BUTTON_RIGHT_BUMPER, GLFW_GAMEPAD_BUTTON_RIGHT_BUMPER)
+		processButton(buttons, GLFW_GAMEPAD_BUTTON_BACK, GLFW_GAMEPAD_BUTTON_BACK)
+		processButton(buttons, GLFW_GAMEPAD_BUTTON_START, GLFW_GAMEPAD_BUTTON_START)
+		processButton(buttons, GLFW_GAMEPAD_BUTTON_GUIDE, GLFW_GAMEPAD_BUTTON_GUIDE)
+		processButton(buttons, GLFW_GAMEPAD_BUTTON_LEFT_THUMB, GLFW_GAMEPAD_BUTTON_LEFT_THUMB)
+		processButton(buttons, GLFW_GAMEPAD_BUTTON_RIGHT_THUMB, GLFW_GAMEPAD_BUTTON_RIGHT_THUMB)
+		processButton(buttons, GLFW_GAMEPAD_BUTTON_DPAD_UP, GLFW_GAMEPAD_BUTTON_DPAD_UP)
+		processButton(buttons, GLFW_GAMEPAD_BUTTON_DPAD_RIGHT, GLFW_GAMEPAD_BUTTON_DPAD_RIGHT)
+		processButton(buttons, GLFW_GAMEPAD_BUTTON_DPAD_DOWN, GLFW_GAMEPAD_BUTTON_DPAD_DOWN)
+		processButton(buttons, GLFW_GAMEPAD_BUTTON_DPAD_LEFT, GLFW_GAMEPAD_BUTTON_DPAD_LEFT)
+//		}
 	}
 
 	/**
