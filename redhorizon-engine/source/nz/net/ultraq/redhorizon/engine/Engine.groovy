12db383cd97c4ffb81d27ae253dcd46462ed12e9
Emanuel Rabina <emanuelrabina@gmail.com>
2026-08-26 21:19:36 +1200

    Add option to remove a system during configuration

--- redhorizon-engine/source/nz/net/ultraq/redhorizon/engine/Engine.groovy
+++ redhorizon-engine/source/nz/net/ultraq/redhorizon/engine/Engine.groovy
@@ -74,6 +74,15 @@ class Engine {
 	}
 
 	/**
+	 * Remove a system from the engine.
+	 */
+	Engine removeSystem(System system) {
+
+		systems.remove(system)
+		return this
+	}
+
+	/**
 	 * Update all of the systems in the engine.
 	 */
 	void update(float delta) {
